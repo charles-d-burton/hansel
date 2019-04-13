c1f82a667b1bbdb703db03832719478521cff20c
charles-d-burton <charles.burton@mindflash.com>
2019-04-13 10:17:43 -0600

    fix: text

--- cmd/controller.go
+++ cmd/controller.go
@@ -30,13 +30,8 @@ type Controller struct {
 // controllerCmd represents the controller command
 var controllerCmd = &cobra.Command{
 	Use:   "controller",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Control machines or the runner",
+	Long:  `Used to issue commands against remotes or schedule commands`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("controller called")
 	},
